f6098ef177fff394aa95b3b459f63180c50ae747
theboyknowsclass <gautam.bhatnagar@theboyknowsclass.com>
2021-01-01 22:02:23 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='smartbekant',
+      version='0.1',
+      description='Control your IKEA BEKANT standing desk with a Raspberry Pi.',
+      url='https://github.com/theboyknowsclass/SmartBekant',
+      author='TheBoyKnowsClass',
+      author_email='g@theboyknowsclass.com',
+      license='GPLv3+',
+      packages=['smartbekant'],
+      zip_safe=False)
